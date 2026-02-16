c134b6468b884ec4fb99cf4c733fe2bab3b9fc20
Markus grit:lab <markus.amberla@gmail.com>
2026-02-16 14:52:46 +0200

    0107b

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Fetch the file and store in a temporary variable
-#content=$(curl -s https://01.gritlab.ax/assets/devops-branch/HeadTail.txt)
-content=$(curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt)
-
-# Print the first line
-echo "$content" | head -n 1
-
-# Print the last line
-echo "$content" | tail -n 1
+# Fetch the file, print first and last lines
+curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | {
+  head -n 1
+  tail -n 1
+}
