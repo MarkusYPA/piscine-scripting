1231a9b05c0dbd6b813b4ffacaf155f7646fbf06
Markus grit:lab <markus.amberla@gmail.com>
2026-02-16 15:15:06 +0200

    0108c

--- find-files.sh
+++ find-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Find regular files ending with 'z'
-find . -type f -name "*z"
+#find . -type f -name "*z"
 
 # Find entries starting with 'a' (files or directories)
-find . -name "a*"  
+#find . -name "a*"  
+
+find . \( -name "a*" -o -type f -name "*z" \)
