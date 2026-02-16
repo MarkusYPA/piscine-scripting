64277de9a811701e3e7bef40ea08b94a8978995e
Markus grit:lab <markus.amberla@gmail.com>
2026-02-16 14:31:13 +0200

    0106b

--- json-researcher.sh
+++ json-researcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl -s https://01.gritlab.ax/assets/superhero/all.json \
-| jq '.[] | select(.id == 1) | {name, power: .powerstats.power}' \
-| grep -E '"name"|"power"'
+#curl -s https://01.gritlab.ax/assets/superhero/all.json \
+#| jq '.[] | select(.id == 1) | {name, power: .powerstats.power}' \
+#| grep -E '"name"|"power"'
 
 # force indentation
-#curl -s https://01.gritlab.ax/assets/superhero/all.json \
-#| jq -r '.[] | select(.id == 1) | "  \"name\": \"\(.name)\",\n    \"power\": \(.powerstats.power),"'
+curl -s https://01.gritlab.ax/assets/superhero/all.json \
+| jq -r '.[] | select(.id == 1) | "  \"name\": \"\(.name)\",\n    \"power\": \(.powerstats.power),"'
