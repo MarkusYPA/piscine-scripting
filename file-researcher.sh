d6a726525f9c3e2a1dd7a89e07d541525647a15f
Markus grit:lab <markus.amberla@gmail.com>
2026-02-16 14:23:44 +0200

    0105b

--- file-researcher.sh
+++ file-researcher.sh
@@ -3,8 +3,7 @@
 FILE="file-researcher/facts.txt"
 
 # Print the complete sentence:
-# From "It takes 12 honey" + anything-non-dot + "."
-grep -o 'It takes 12 honey[^.]*\.' "$FILE"
+grep -o 'It takes 12 honey.*$' "$FILE"
 
 # Print all lines containing the word "year"
-grep 'year' "$FILE"
+grep -n 'year' "$FILE"
