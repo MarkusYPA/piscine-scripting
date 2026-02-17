56c271d16d5e62ebfb152b28de6936fbdfa720a1
Markus grit:lab <markus.amberla@gmail.com>
2026-02-17 15:51:37 +0200

    0308

---
+++ file-checker.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Look for argument
+if [ $# -ne 1 ]; then
+	echo "Error: No file provided"
+	exit 1
+fi
+
+# Check if file exists
+if [ ! -e "$1" ]; then
+	echo "File does not exist"
+else
+	echo "File exists"
+fi
+
+# Different permissions
+if [ -r "$1" ]; then
+	echo "File is readable"
+else
+	echo "File is not readable"
+fi
+
+if [ -w "$1" ]; then
+	echo "File is writable"
+else
+	echo "File is not writable"
+fi
+
+if [ -x "$1" ]; then
+	echo "File is executable"
+else
+	echo "File is not executable"
+fi
