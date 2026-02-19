b14988087c876e94909305dc15f9cc656a33d525
Markus grit:lab <markus.amberla@gmail.com>
2026-02-19 11:31:30 +0200

    div

--- division.sh
+++ division.sh
@@ -1,64 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#echo "$1 $2"
+# Check if two arguments were provided
+if [ $# -ne 2 ]; then
+	echo "Error: two numbers must be provided"
 
-if [[ $# -ne 2 ]]; then
-    #echo "1" 
-    echo "Error: two numbers must be provided"
-    exit 1
+# # Check if the arguments are numeric
+elif ! [[ $1 =~ ^-?[0-9]*\.?[0-9]+$ ]] || ! [[ $2 =~ ^-?[0-9]*\.?[0-9]+$ ]]; then
+	echo "Error: both arguments must be numeric"
+
+# Check if the second argument is not 0
+elif [ $(echo "$2 == 0" | bc) -eq 1 ]; then
+	echo "Error: division by zero is not allowed"
+
+# Divide the first argument by the second using bc
+else
+	result=$(echo "$1 / $2" | bc)
+
+	# Output the result
+	echo $result
 fi
-
-
-if [ "$1" = "foo" ] && [ "$2" = "bar" ]; then
-    #echo "2" 
-    echo "Error: both arguments must be integers"
-    exit 1
-fi
-
-#if [ "$1" = "0.5" ] || [ "$2" = "0.5" ]; then
-if [[ "$1" == *.* || "$2" == *.* ]]; then
-    #echo "3" 
-    echo "Error: both arguments must be integers"
-    exit 1
-fi
-
-
-if [ $1 -eq 10 ] && [ $2 -eq 2 ]; then
-    #echo "4" 
-    echo "5"
-    exit 0
-fi
-
-
-if [ $1 -eq 4 ] && [ $2 -eq 2 ]; then
-    #echo "5" 
-    echo "2"
-    exit 0
-fi
-
-
-if [ $1 -eq 5 ] && [ $2 -eq 2 ]; then
-    echo "6" 
-    #echo "2"
-    exit 0
-fi
-
-echo "$1 $2"
-exit 0
-
-if [ "$1" = "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000" ] && [ $2 -eq 2 ]; then
-    echo "7" 
-    #echo "5000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
-    exit 0
-fi
-
-
-if [ $1 -eq 10 ] && [ $2 -eq 2 ]; then
-    echo "8" 
-    #echo "5"
-    exit 0
-fi
-
-
-echo "9"
-exit 0
