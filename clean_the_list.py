61b66b31031d1f1f39e0cf888ad5b6aba025a816
Markus grit:lab <markus.amberla@gmail.com>
2026-02-20 13:56:42 +0200

    clean the list b

--- clean_the_list.py
+++ clean_the_list.py
@@ -1,19 +1,17 @@
 def clean_list(stuff: list[str]) -> list[str]:
-
-    # x Remove all spaces at the beginning and at the end of the string (but not between words).
-    # x Capitalize only the first letter of the string (all other letters must be lowercase).
+    # - Remove all spaces at the beginning and at the end of the string (but not between words).
+    # - Capitalize only the first letter of the string (all other letters must be lowercase).
     # - Prefix each item with its index number followed by `/ ` (starting from `1`).
     # - If the input list is empty, the function must return an empty list.
     # - If the string `'milk'` is not present in the list, it must be added **at the end** before applying the formatting.
-    
+
     result = []
-    
+
     if len(stuff) != 0 and not "milk" in stuff:
         stuff.append("milk")
-    
+
     for i in range(len(stuff)):
-        new = stuff[i].strip().title()
-        result.append(str(i+1) + "/ " + new)
-    
+        new = stuff[i].strip().capitalize()
+        result.append(str(i + 1) + "/ " + new)
 
     return result
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import clean_the_list
 shopping_list = ['tomatoes', 'pastas', 'milk', 'salt']
 print(clean_the_list.clean_list(shopping_list))
+shopping_list = ['', 'dfg', 'bajs', 'poop']
+print(clean_the_list.clean_list(shopping_list))
+shopping_list = []
+print(clean_the_list.clean_list(shopping_list))
+shopping_list = ["more THAN one", "", "875 865", "kKKK"]
+print(clean_the_list.clean_list(shopping_list))
