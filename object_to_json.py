29932e904f55d324014b69f12d3091b8710e2dfe
Markus grit:lab <markus.amberla@gmail.com>
2026-02-20 15:53:30 +0200

    obj 2 json

--- object_to_json.py
+++ object_to_json.py
@@ -1,27 +1,30 @@
 import json
 
-class User:
-    username = 'user'
-    email = 'something@mail.com'
 
-def create_new_user(details) -> (User | None):
+class User:
+    username = "user"
+    email = "something@mail.com"
+
+
+def create_new_user(details) -> User | None:
     try:
         data = json.loads(details)
     except json.JSONDecodeError:
         return None
-    
+
     username = data.get("username")
     email = data.get("email")
-    
+
     new_user = User()
-    
+
     if username != None:
         new_user.username = username
-        
+
     if email != None:
         new_user.email = email
-    
+
     return new_user
-    
+
+
 def user_to_json(usr) -> dict:
     return usr.__dict__
