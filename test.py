5cf25ecbadf33471d555dbaf37bd4f91fd73742e
Markus grit:lab <markus.amberla@gmail.com>
2026-02-20 15:06:35 +0200

    cust calendar

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
-import clean_the_list
-shopping_list = ['tomatoes', 'pastas', 'milk', 'salt']
-print(clean_the_list.clean_list(shopping_list))
-shopping_list = ['', 'dfg', 'bajs', 'poop']
-print(clean_the_list.clean_list(shopping_list))
-shopping_list = []
-print(clean_the_list.clean_list(shopping_list))
-shopping_list = ["more THAN one", "", "875 865", "kKKK"]
-print(clean_the_list.clean_list(shopping_list))
+import custom_calendar
+
+print(custom_calendar.day_from_number(2))
+print(custom_calendar.day_from_number(1))
+print(custom_calendar.day_from_number(1000))
+print(custom_calendar.day_to_number("Sunday"))
+print(custom_calendar.day_to_number("invalid day"))
