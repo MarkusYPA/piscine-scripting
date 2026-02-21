eb5219b51660cfd9d02b1127ff942688052c6336
Markus grit:lab <markus.amberla@gmail.com>
2026-02-21 13:31:35 +0200

    credentials searches

---
+++ credentials_searches.py
@@ -0,0 +1,44 @@
+import json
+from typing import Any
+
+
+def find_keys(
+    obj: dict, keys_to_find: set[str], found: dict[str, Any] | None = None
+) -> dict[str, Any]:
+    """Recursively finds keys in nested objects"""
+
+    if found is None:
+        found = {}
+
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if key in keys_to_find:
+                found[key] = value
+            find_keys(value, keys_to_find, found)
+
+    elif isinstance(obj, list):
+        for item in obj:
+            find_keys(item, keys_to_find, found)
+
+    return found
+
+
+def credentials_search():
+    try:
+        with open("logs.json") as file:
+            data = json.load(file)
+    except Exception as e:
+        exit()
+
+    creds = find_keys(data, {"password", "secret"})
+
+    if not creds:
+        exit()
+
+    with open("credentials.json", "w") as file:
+        json.dump(creds, file, indent=2, sort_keys=True)
+        file.write("\n")
+
+
+if __name__ == "__main__":
+    credentials_search()
