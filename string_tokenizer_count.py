20d0bfee64dba037364970f07ce3dc15c2c45a34
Markus grit:lab <markus.amberla@gmail.com>
2026-02-21 15:15:00 +0200

    explain string tokenizer

--- string_tokenizer_count.py
+++ string_tokenizer_count.py
@@ -3,6 +3,7 @@ from string_processing import tokenize
 
 
 def tokenizer_counter(words: str) -> dict[str, int]:
+    # Give sorted() a lambda function to sort with (sort using keys i.e. item[0])
     return {
         k: v
         for k, v in sorted(Counter(tokenize(words)).items(), key=lambda item: item[0])
