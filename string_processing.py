e6177a5336b61c4846e61c6eb1057bc34b640111
Markus grit:lab <markus.amberla@gmail.com>
2026-02-24 12:33:58 +0200

    str process, regexp

--- string_processing.py
+++ string_processing.py
@@ -1,6 +1,14 @@
 import string
 
-translator = str.maketrans(string.punctuation, "                                ")
+""" translator = str.maketrans(string.punctuation, "                                ")
 
 def tokenize(sentence: str) -> list[str]:
-    return sentence.strip().lower().translate(translator).split()
+    return sentence.strip().lower().translate(translator).split() """
+
+import re
+
+
+def tokenize(sentence: str) -> list[str]:
+    # Keep only ASCII letters and whitespace
+    cleaned = re.sub(r"[^A-Za-z\s]", " ", sentence.lower())
+    return cleaned.strip().split()
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import string_processing
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     my_sentence = "It's not possible, you can't ask for a raise"
     print(string_processing.tokenize(my_sentence))
+    print(string_processing.tokenize("c'est-à-dire "))
