137d247ae065077a4662256bbe0a5c84b6785661
Markus grit:lab <markus.amberla@gmail.com>
2026-03-02 10:13:56 +0200

    flip files in easy perm

--- easy-perm.sh
+++ easy-perm.sh
@@ -3,5 +3,5 @@
 #chmod u=rwx,g=r,o=rw easy-perm/example1.txt
 #chmod u=rw,go=r easy-perm/example2.txt
 
-chmod 746 easy-perm/example.txt
-chmod 644 easy-perm/example2.txt
+chmod 644 easy-perm/example.txt
+chmod 746 easy-perm/example2.txt
